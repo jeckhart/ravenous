5c0f4bb670bb8463aa7ff976cb63493f83f78c92
John Eckhart <johne@nearpod.com>
2018-02-12 22:59:56 -0500

    Fix Yelp API and move API key to environment variable

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,28 +1,11 @@
-const clientID = 'h67lwlZSeKAHCm2GPJ9X4A';
-const secret = 'CiA7tz5o5YohT4AA8PwIgjUnr5WhGo9wryopWfFw6zIczU9KMtdKfe2K7ijXCIXS';
-let accessToken;
 
+const api_key = process.env.REACT_APP_API_KEY;
 
 const Yelp = {
-  getAccessToken() {
-    if(accessToken) {
-      return new Promise(resolve =>
-      resolve(accessToken));
-    }
-
-    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=${clientID}&client_secret=${secret}`,
-    {method: 'POST'}).then(response => {
-      return response.json();
-    }).then(jsonResponse => {
-      accessToken = jsonResponse.access_token;
-    });
-  },
 
   search(term, location, sortBy) {
-    return Yelp.getAccessToken().then(() => {
-      return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
-      {headers: {Authorization: `Bearer${accessToken}`}
-      });
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+      {headers: {Authorization: `Bearer ${api_key}`}
     }).then(response => {
       return response.json();
     }).then(jsonResponse => {
